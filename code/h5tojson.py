664497488a9c53abf163b928fa9363398fe235f3
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-05-19 11:16:12 +0200

    Added hdf5_getters

--- code/h5tojson.py
+++ code/h5tojson.py
@@ -1,6 +1,10 @@
 import json, h5py, sys, os
+import hdf5_getters
 
 dir = sys.argv[1]
+
+def dump_file(f):
+	f.write(json.dumps(f[x].visit))
 
 for filename in os.listdir(dir):
 	filename = dir+filename
@@ -14,11 +18,15 @@ for filename in os.listdir(dir):
 	with h5py.File(filename, "r") as f:
 		my_keys_list=list(f.keys())
 		for x in my_keys_list:
+			f.visit(dump_file)
 			print(list(f[x]))
+			print(type(f[x]))
 
-	# dump the data into json
-	with open('data.json','a') as f_out:
-		 with h5py.File(filename, "r") as f:
-				 my_keys_list=list(f.keys())
-				 for x in my_keys_list:
-						f_out.write(json.dumps(f[x]))
+
+
+	# # dump the data into json
+	# with open('data.json','a') as f_out:
+	# 	 with h5py.File(filename, "r") as f:
+	# 			 my_keys_list=list(f.keys())
+	# 			 for x in my_keys_list:
+	# 					f_out.write(json.dumps(f[x].visit))
